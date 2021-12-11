04953ce30e5a51115dc484cbe2833200a6243bf6
TokenMinal <ludocrashnadeau@gmail.com>
2021-12-11 13:40:06 -0500

    HELP ME

---
+++ Lecteur de musique/script.js
@@ -0,0 +1,14 @@
+const musiqueContenant = document.getElementById('musique-contenant');
+
+const btnPlay = document.getElementById('play');
+const btnPredecedent = document.getElementById('precedent');
+const btnSuivant = document.getElementById('suivant');
+
+const audio = document.getElementById('progres-content');
+const progres = document.getElementById('progres');
+const progresContent = document.getElementById('progres-contenant');
+
+const titre = document.getElementById('titre');
+const cover = document.getElementById('cover');
+
+const chasonsArr = ['Going Higher', 'High Octave', 'Sexy'];
